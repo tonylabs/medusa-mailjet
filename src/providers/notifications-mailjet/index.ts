97cef56d15588c493362d8cf44f1d1fea38ee7ff
TONYLABS <tony.wang@tonylabs.com>
2025-11-16 15:41:05 +0800

    Update index.ts

--- src/providers/notifications-mailjet/index.ts
+++ src/providers/notifications-mailjet/index.ts
@@ -1,5 +1,5 @@
 import MailjetNotificationProviderService from "./service"
-import {ModuleProvider, Modules} from "@medusajs/framework/utils"
+import { ModuleProvider, Modules } from "@medusajs/framework/utils"
 
 export default ModuleProvider(Modules.NOTIFICATION, {
   services: [MailjetNotificationProviderService],
